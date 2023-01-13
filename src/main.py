aea148092fe8292a9ef0e49f5adeb2fa28c1d82b
Lumbril <32036622+Lumbril@users.noreply.github.com>
2023-01-13 14:20:49 +0300

    added create table

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+from database import Base
 from routers import test
+from settings import engine
 
 
 app = FastAPI()
 app.include_router(test.test_api)
+
+Base.metadata.create_all(bind=engine)
