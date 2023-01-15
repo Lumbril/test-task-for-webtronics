f21d83153d7350934df09d0a7aa1fef4ba6a3d68
Lumbril <32036622+Lumbril@users.noreply.github.com>
2023-01-15 22:05:21 +0300

    initial settings for docker

--- src/settings/prod.py
+++ src/settings/prod.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_NAME = os.get('DB_NAME', 'webtronics')
-DB_USER = os.get('DB_USER', 'user')
-DB_PASSWORD = os.get('DB_PASSWORD', 'password')
-DB_HOST = os.get('DB_HOST', 'localhost')
+DB_NAME = os.getenv('DB_NAME', 'webtronics')
+DB_USER = os.getenv('DB_USER', 'user')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
+DB_HOST = os.getenv('DB_HOST', 'localhost')
 
 SQLALCHEMY_DATABASE_URL = (
     f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}'
