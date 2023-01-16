e906ced580e39875519b37a0a3b1d36aa9cdda1f
Lumbril <32036622+Lumbril@users.noreply.github.com>
2023-01-16 15:20:01 +0300

    added crud for posts

--- src/database/crud/post_crud.py
+++ src/database/crud/post_crud.py
@@ -8,8 +8,14 @@ def get_post(db: Session, post_id: int):
     return db.query(models.PostModel).filter(models.PostModel.id == post_id).first()
 
 
-def get_post_by_author(db: Session, author: str):
-    return db.query(models.PostModel).filter(models.PostModel.author == author).first()
+def get_post_by_author(db: Session, post_id: int, author: int):
+    return db.query(models.PostModel).filter(models.PostModel.id == post_id,
+                                             models.PostModel.author == author).first()
+
+
+def get_posts_by_author(db: Session, author: str, skip: int = 0, limit: int = 100):
+    return db.query(models.PostModel).filter(models.PostModel.author == author). \
+            offset(skip).limit(limit).all()
 
 
 def get_posts(db: Session, skip: int = 0, limit: int = 100):
@@ -22,5 +28,25 @@ def create_post(db: Session, post: schemas.PostCreateSchema, author: UserModel):
     db.add(db_post)
     db.commit()
     db.refresh(db_post)
+
+    return db_post
+
+
+def update_post(db: Session, post: models.PostModel, post_update: schemas.PostCreateSchema):
+    update_data = post_update.dict(exclude_unset=True)
+
+    for key, value in update_data.items():
+        setattr(post, key, value)
+
+    db.commit()
+
+    return post
+
+
+def delete_post(db: Session, post_id: int):
+    db_post = get_post(db, post_id)
+
+    db.delete(db_post)
+    db.commit()
 
     return db_post
--- src/routers/post.py
+++ src/routers/post.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from database import crud
@@ -10,7 +12,45 @@ from services.token import get_active_user_by_token
 post_api = APIRouter()
 
 
+@post_api.get('/post', response_model=List[PostSchema])
+def get_post_list(user: UserModel = Depends(get_active_user_by_token), db: Session = Depends(get_db)):
+    return crud.get_posts_by_author(db, user.id)
+
+
+@post_api.get('/post/{post_id}', response_model=PostSchema)
+def get_post(post_id: int, user: UserModel = Depends(get_active_user_by_token), db: Session = Depends(get_db)):
+    post = crud.get_post_by_author(db, post_id, user.id)
+
+    if post is None:
+        raise HTTPException(status_code=400, detail="Post is not exists")
+
+    return post
+
+
 @post_api.post('/post', response_model=PostSchema)
 def create_post(data: PostCreateSchema, user: UserModel = Depends(get_active_user_by_token),
                 db: Session = Depends(get_db)):
     return crud.create_post(db, data, user)
+
+
+@post_api.put('/post/{post_id}', response_model=PostSchema)
+def update_post(post_id: int, data: PostCreateSchema, user: UserModel = Depends(get_active_user_by_token),
+                db: Session = Depends(get_db)):
+    post = crud.get_post_by_author(db, post_id, user.id)
+
+    if post is None:
+        raise HTTPException(status_code=400, detail="Post is not exists")
+
+    return crud.update_post(db, post, data)
+
+
+@post_api.delete('/post/{post_id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(post_id: int, user: UserModel = Depends(get_active_user_by_token),
+                db: Session = Depends(get_db)):
+
+    if crud.get_post_by_author(db, post_id, user.id) is None:
+        raise HTTPException(status_code=400, detail="Post is not exists")
+
+    crud.delete_post(db, post_id)
+
+    return None
