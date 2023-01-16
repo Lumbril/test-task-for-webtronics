61183c21633d7cbe2a18a9e897466915f5537a20
Lumbril <32036622+Lumbril@users.noreply.github.com>
2023-01-16 19:49:49 +0300

    test redis

--- src/routers/test.py
+++ src/routers/test.py
@@ -4,6 +4,9 @@ from database.models import UserModel
 from database.schemas import UserSchema
 from services.token import get_active_user_by_token
 
+import redis
+
+
 test_api = APIRouter(tags=['Test'])
 
 
@@ -15,3 +18,12 @@ async def test():
 @test_api.get('/test/me', response_model=UserSchema)
 def me(user: UserModel = Depends(get_active_user_by_token)):
     return user
+
+
+@test_api.get('/test/redis')
+def test_redis():
+    r = redis.Redis(host='redis')
+    r.mset({"Croatia": "Zagreb", "Bahamas": "Nassau"})
+    x = r.get("Bahamas")
+
+    return x if x else 'bad'
