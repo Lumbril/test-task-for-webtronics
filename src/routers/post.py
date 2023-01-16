1d16f414484fa8dd4bc64fbd1d3cfa3cea273e22
Lumbril <32036622+Lumbril@users.noreply.github.com>
2023-01-16 16:02:46 +0300

    Update post.py

--- src/routers/post.py
+++ src/routers/post.py
@@ -9,16 +9,26 @@ from database.models import UserModel
 from database.schemas import PostSchema, PostCreateSchema
 from services.token import get_active_user_by_token
 
-post_api = APIRouter()
+post_api = APIRouter(prefix='/post')
 
 
-@post_api.get('/post', response_model=List[PostSchema])
-def get_post_list(user: UserModel = Depends(get_active_user_by_token), db: Session = Depends(get_db)):
+@post_api.get('', response_model=List[PostSchema])
+def get_all_posts(db: Session = Depends(get_db)):
+    return crud.get_posts(db)
+
+
+@post_api.get('/{post_id}', response_model=PostSchema)
+def get_post(post_id: int, db: Session = Depends(get_db)):
+    return crud.get_post(db, post_id)
+
+
+@post_api.get('/my/', response_model=List[PostSchema])
+def get_my_post_list(user: UserModel = Depends(get_active_user_by_token), db: Session = Depends(get_db)):
     return crud.get_posts_by_author(db, user.id)
 
 
-@post_api.get('/post/{post_id}', response_model=PostSchema)
-def get_post(post_id: int, user: UserModel = Depends(get_active_user_by_token), db: Session = Depends(get_db)):
+@post_api.get('/my/{post_id}', response_model=PostSchema)
+def get_my_post(post_id: int, user: UserModel = Depends(get_active_user_by_token), db: Session = Depends(get_db)):
     post = crud.get_post_by_author(db, post_id, user.id)
 
     if post is None:
@@ -27,13 +37,13 @@ def get_post(post_id: int, user: UserModel = Depends(get_active_user_by_token), 
     return post
 
 
-@post_api.post('/post', response_model=PostSchema)
+@post_api.post('/my', response_model=PostSchema)
 def create_post(data: PostCreateSchema, user: UserModel = Depends(get_active_user_by_token),
                 db: Session = Depends(get_db)):
     return crud.create_post(db, data, user)
 
 
-@post_api.put('/post/{post_id}', response_model=PostSchema)
+@post_api.put('/my/{post_id}', response_model=PostSchema)
 def update_post(post_id: int, data: PostCreateSchema, user: UserModel = Depends(get_active_user_by_token),
                 db: Session = Depends(get_db)):
     post = crud.get_post_by_author(db, post_id, user.id)
@@ -44,7 +54,7 @@ def update_post(post_id: int, data: PostCreateSchema, user: UserModel = Depends(
     return crud.update_post(db, post, data)
 
 
-@post_api.delete('/post/{post_id}', status_code=status.HTTP_204_NO_CONTENT)
+@post_api.delete('/my/{post_id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(post_id: int, user: UserModel = Depends(get_active_user_by_token),
                 db: Session = Depends(get_db)):
 
